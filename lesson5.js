62cf5e62cf824eeae3d03e62e28512145feee273
Mirko40 <mmirko@ua.fm>
2022-08-28 22:09:37 +0300

    Initial

--- lesson5.js
+++ lesson5.js
@@ -1,5 +1,20 @@
 "use strict"
 
 let car = {
-
+    mark: "Skoda",
+    model: "Oktavia",
+    year: 2007,
+    "average speed": 120,
 };
+function infoCar() {
+    for (let key in car) {
+        console.log(key)
+        console.log(car[key])
+    }
+}
+function getTimeRoad(l) {
+    return l / (car["average speed"])
+}
+infoCar()
+let a = getTimeRoad(200)
+console.log(a)
